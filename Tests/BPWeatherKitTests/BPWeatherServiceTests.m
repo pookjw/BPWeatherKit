72b79f7df78115383c7193cf2c96bac064bc0261
Jinwoo Kim <kidjinwoo@me.com>
2023-03-26 21:03:27 +0900

    no message

--- Tests/BPWeatherKitTests/BPWeatherServiceTests.m
+++ Tests/BPWeatherKitTests/BPWeatherServiceTests.m
@@ -6,20 +6,43 @@
 //
 
 #import "BPWeatherServiceTests.h"
+#import <CoreLocation/CoreLocation.h>
 @import BPWeatherKit;
+
+@interface BPWeatherServiceTests ()
+@property (retain) BPWeatherService *weatherService;
+@end
 
 @implementation BPWeatherServiceTests
 
+- (void)dealloc {
+    [_weatherService release];
+    [super dealloc];
+}
+
 - (void)setUp {
+    self.weatherService = BPWeatherService.sharedInstance;
     [super setUp];
 }
 
 - (void)tearDown {
+    self.weatherService = nil;
     [super tearDown];
 }
 
 - (void)test_getWeather {
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:37.570013917406 longitude:126.9780542555];
     
+    XCTestExpectation *expectation = [[XCTestExpectation alloc] initWithDescription:[NSString string]];
+    
+    [self.weatherService weatherForLocation:location completionHandler:^(BPWeather * _Nullable weather, NSError * _Nullable error) {
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    
+    [location release];
+    [self waitForExpectations:@[expectation] timeout:10.f];
+    [expectation release];
 }
 
 @end
